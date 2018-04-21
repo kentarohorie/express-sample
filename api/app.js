e63ad891a6735c5afdb83fc7148778950ed7ec0c
kentarohorie <kentaro.horie.4@gmail.com>
2018-04-21 19:33:51 +0900

    Change Directory Tree

--- api/app.js
+++ api/app.js
@@ -5,8 +5,10 @@ var bodyParser = require('body-parser')
 var routes = require('./routes/index');
 var questions = require('./routes/questions');
 
+app.set('views', __dirname + './views')
 app.use(bodyParser())
 app.use('/', routes);
 app.use('/questions', questions)
+
 
 module.exports = app;
